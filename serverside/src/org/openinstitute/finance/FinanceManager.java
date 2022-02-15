ba6d1bfaccebba3878b460d28124a29c2d48d310
Cristobal Mejia <cristobal@openedit.org>
2022-02-15 13:32:06 -0600

    Balance Report (Expenses)

--- serverside/src/org/openinstitute/finance/FinanceManager.java
+++ serverside/src/org/openinstitute/finance/FinanceManager.java
@@ -3,18 +3,22 @@ package org.openinstitute.finance;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.entermedia.transactions.TransactionManager;
 import org.entermediadb.asset.MediaArchive;
+import org.entermediadb.elasticsearch.SearchHitData;
 import org.openedit.CatalogEnabled;
 import org.openedit.Data;
 import org.openedit.ModuleManager;
 import org.openedit.data.QueryBuilder;
 import org.openedit.data.Searcher;
 import org.openedit.hittracker.HitTracker;
+import org.openedit.hittracker.ListHitTracker;
 
 public class FinanceManager  implements CatalogEnabled
 {
@@ -84,14 +88,58 @@ public class FinanceManager  implements CatalogEnabled
 		return null;
 	}
 	
-	public HitTracker   getExpenseTypesByDateRange(String inCollectionId, DateRange inDateRange) 
+	public Map   getExpenseTypesByDateRange(String inCollectionId, DateRange inDateRange) 
 	{
 		Searcher expensesSearcher = getMediaArchive().getSearcher("collectiveexpense");
 		QueryBuilder query = expensesSearcher.query();
 		query.exact("collectionid", inCollectionId);
 		HitTracker hits = expensesSearcher.search(query.getQuery());
+		Collection pageOfHits = hits.getPageOfHits();
+		pageOfHits = new ArrayList(pageOfHits); 
 		
-		return hits;
+		HashMap<String, Object> bycurrency = new HashMap<String, Object>();
+		
+		for (Iterator iterator = pageOfHits.iterator(); iterator.hasNext();) {
+			SearchHitData data = (SearchHitData) iterator.next();
+			String currency = (String) data.getValue("currencytype");
+			
+			Collection values = (Collection) bycurrency.get(currency);
+			if( values == null)
+			{
+				values = new ListHitTracker();
+				//values.add(data);
+				bycurrency.put(currency, values);
+			}
+			values.add(data);
+			
+		}
+		//summarize by ExpenseType
+		String currentcurrency = "";
+		for (Map.Entry<String, Object> set :bycurrency.entrySet()) {
+			Collection data = (Collection) set.getValue();
+			HashMap<String, Object> currenttype = new HashMap<String, Object>();
+			String currenttypeid = "";
+			for (Iterator iterator = data.iterator(); iterator.hasNext();) {
+				SearchHitData row = (SearchHitData) iterator.next();
+				currenttypeid = (String) row.getValue("expensetype");
+				
+				SearchHitData values = (SearchHitData)currenttype.get(currenttypeid);
+				if (values == null) {
+					values = new SearchHitData();
+					currenttype.put(currenttypeid, values);
+					values.setValue("total", 0.0);
+				}
+				
+				values.setValue("total", (Double) values.getValue("total") + (Double) row.getValue("total"));
+				values.setValue("currencytype", (String) row.getValue("currencytype"));
+				values.setValue("expensetype", currenttypeid);
+
+			}
+			bycurrency.replace(set.getKey(), currenttype);
+			
+		}
+			
+		return bycurrency;
 	}
 	
 		
