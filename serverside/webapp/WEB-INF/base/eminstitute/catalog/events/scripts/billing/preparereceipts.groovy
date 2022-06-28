abae4b8cb10e0ba0a9d2336d9dcc04a32e58f4ec
Cristobal Mejia <cristobal@openedit.org>
2022-06-27 19:22:42 -0500

    prepare table

--- serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/billing/preparereceipts.groovy
+++ serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/billing/preparereceipts.groovy
@@ -5,21 +5,34 @@ import org.entermediadb.asset.MediaArchive
 import org.openedit.*
 import org.openedit.data.Searcher
 import org.openedit.users.User
+import org.openedit.hittracker.HitTracker
+
 
 public void init() {
 	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
 
 	Searcher transactionSearcher = mediaArchive.getSearcher("transaction");
-
-	Collection pendingNotification = transactionSearcher.query()
-									.match("receiptstatus","new").search();
+	ArrayList tosave = new ArrayList();
+	HitTracker pendingNotification = transactionSearcher.query()
+			.exact("receiptstatus","sent").search();
+			
+	pendingNotification.enableBulkOperations();
 	
-	for (Iterator receiptIterator = pendingNotification.iterator(); receiptIterator.hasNext();) {
-		Data receipt = transactionSearcher.loadData(receiptIterator.next());
+	for (Data receipt in pendingNotification) {
+		
 		receipt.setValue("receiptstatus", "");
-		transactionSearcher.saveData(receipt);
+		//transactionSearcher.saveData(receipt);
+		
+		tosave.add(receipt);
+		if(tosave.size() == 100)
+			{
+				transactionSearcher.saveAllData( tosave, null );
+				log.info(pendingNotification.size() + " receipts updated");
+				tosave.clear();
+			}
 
 	}
+	transactionSearcher.saveAllData( tosave, null );
 	log.info(pendingNotification.size() + " receipts updated");
 }
 
