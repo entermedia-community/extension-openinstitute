aaf1ed129ed8cfd0549a42f238316122ca179520
Christopher Burkey <cburkey@entermediadb.org>
2024-02-29 11:16:09 -0600

    Domain

--- serverside/src/org/openinstitute/server/OpenInstituteModule.java
+++ serverside/src/org/openinstitute/server/OpenInstituteModule.java
@@ -1,13 +1,20 @@
 package org.openinstitute.server;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.entermediadb.asset.MediaArchive;
 import org.entermediadb.asset.modules.BaseMediaModule;
+import org.entermediadb.asset.util.JsonUtil;
 import org.entermediadb.projects.LibraryCollection;
 import org.entermediadb.projects.ProjectManager;
 import org.openedit.Data;
 import org.openedit.WebPageRequest;
+import org.openedit.data.QueryBuilder;
+import org.openedit.hittracker.HitTracker;
+import org.openedit.hittracker.SearchQuery;
+import org.openedit.hittracker.Term;
+import org.openedit.profile.UserProfile;
 import org.openedit.servlet.SiteData;
 import org.openedit.util.PathUtilities;
 
@@ -48,7 +55,23 @@ public class OpenInstituteModule extends BaseMediaModule
 //
 //	}
 	
-	
+	public Data loadCommunityTagByDomain(WebPageRequest inReq)
+	{
+		MediaArchive archive = getMediaArchive(inReq);
+		Collection tags = archive.getList("communitytagcategory");
+		String url = inReq.getSiteRoot();
+		for (Iterator iterator = tags.iterator(); iterator.hasNext();)
+		{
+			Data tag = (Data) iterator.next();
+			String domain = tag.get("externaldomain");
+			if(domain != null && url.contains( domain ) )
+			{
+				inReq.putPageValue("communitytagcategory", tag);
+				return tag;
+			}
+		}
+		return null;
+	}
 	
 	public void loadCommunityTagFolder(WebPageRequest inReq)
 	{
@@ -64,34 +87,63 @@ public class OpenInstituteModule extends BaseMediaModule
 		}
 	}
 
-	public void loadCommunityTagCategory(WebPageRequest inReq)
+	public Data loadCommunityTagCategory(WebPageRequest inReq)
 	{
 		MediaArchive archive = getMediaArchive(inReq);
 		String communitytagcategory = inReq.getRequestParameter("communitytagcategory");
+		Data tag = null;
 		if( communitytagcategory !=null)
 		{
-			Data tag = archive.getCachedData("communitytagcategory", communitytagcategory);
-			if(tag != null) {
-				inReq.putPageValue("communitytagcategory", tag);
-				inReq.putPageValue("communitytagcategoryid", tag.getId());
-				return;
-			}
-			
+			tag = archive.getCachedData("communitytagcategory", communitytagcategory);
 		}
-		SiteData data = (SiteData)inReq.getPageValue("sitedata");
-		if( data != null)
+		if( tag == null)
 		{
-			String tagid = data.get("domaincommunityid");
-			if( tagid != null)
+			SiteData data = (SiteData)inReq.getPageValue("sitedata");
+			if( data != null)
 			{
-				Data tag = archive.getCachedData("communitytagcategory", tagid);
-				if(tag != null) {
-					inReq.putPageValue("communitytagcategory", tag);
-					inReq.putPageValue("communitytagcategoryid", tag.getId());
-					return;
+				String tagid = data.get("domaincommunityid");
+				if( tagid != null)
+				{
+					tag = archive.getCachedData("communitytagcategory", tagid);
 				}
 			}
 		}
+		if(tag != null) {
+			inReq.putPageValue("communitytagcategory", tag);
+			inReq.putPageValue("communitytagcategoryid", tag.getId());
+		}
+		return tag;
 	}
+
+	public void loadCommunityBlog(WebPageRequest inReq)
+	{
+		Data tag = loadCommunityTagByDomain(inReq);
+
+		Collection collections = getMediaArchive(inReq).query("librarycollection").exact("communitytagcategory",tag).search(inReq);
+		
+		QueryBuilder builder = getMediaArchive(inReq).query("userupload");
+		
+		HitTracker topuploads = null;
+		builder.exact("exclusivecontent", false);
+		if( collections.isEmpty())
+		{
+			builder.exact("librarycollection", "NONE");
+		}
+		else 
+		{
+			builder.orgroup("librarycollection", collections);
+		}
+		builder.all();
+		topuploads = builder.named("topuploads").sort("uploaddateDown").search(inReq);
+//			
+//			String page = inReq.getRequestParameter("page");
+//			if( page != null)
+//			{
+//				topuploads.setPage(Integer.parseInt(page));
+//			}
+		inReq.putPageValue("topuploads", topuploads);
+
+	}
+
 	
 }
