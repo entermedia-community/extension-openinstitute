0f56f87e5cf6ce83b3d7a542ac62b7dc6ef7a282
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2024-03-31 20:45:06 +0600

    Add new files and update navigation links

--- serverside/webapp/WEB-INF/base/eminstitute/community/emedia/js/script.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/emedia/js/script.js
@@ -6,70 +6,29 @@ $(document).ready(function () {
       $(".navigation").removeClass("fixed-menu ");
     }
   });
-
-  $(".brand-logo-slider").slick({
-    dots: false,
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    fade: false,
-    speed: 300,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 991,
-
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          dots: false,
-          infinite: true,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          dots: false,
-          infinite: true,
-          arrows: false,
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+  $("#test").click(function () {
+    $("#collapseTutorial").collapse("toggle");
   });
-
-  $("#datetimepicker").datetimepicker({
-    inline: true,
-    minDate: new Date(),
-    maxDate: new Date(new Date().setDate(new Date().getDate() + 30)),
-  });
-
-  $("input[name=type]").on("change", function () {
-    var val = $(this).val();
-    if (val == "video") {
-      $(".conf-link").show();
+  $(".prop-pse .nav-link").on("click", function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+    var target = $(this).data("target");
+    var propPse = $(this).closest(".prop-pse");
+    if (target === "problem") {
+      propPse.removeClass("emp");
+      propPse.addClass("prb");
     } else {
-      $(".conf-link").hide();
+      propPse.removeClass("prb");
+      propPse.addClass("emp");
     }
   });
 });
 
 $(window).on("load", function () {
-  AOS.init({
-    duration: 600,
-    once: true,
-  });
+  if (AOS in window) {
+    AOS.init({
+      duration: 600,
+      once: true,
+    });
+  }
 });
