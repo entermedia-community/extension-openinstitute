275f24c1037459e7b98ae5f972064481312e2645
Christopher Burkey <cburkey@entermediadb.org>
2024-06-22 16:55:02 -0600

    Links

--- serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/billing/savedproduct.groovy
+++ serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/billing/savedproduct.groovy
@@ -3,11 +3,19 @@ package billing;
 import org.entermediadb.asset.MediaArchive
 import org.openedit.*
 import org.openedit.data.Searcher
+import org.openedit.util.PathUtilities
 
 public void init() {
 	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
 	Searcher productSearcher = mediaArchive.getSearcher("collectiveproduct");
 	Data product = context.getPageValue("data");
+	
+	if( product.getValue("urlname") == null)
+	{
+		String dash = PathUtilities.dash(product.getName());
+		product.setValue("urlname",dash)
+		productSearcher.saveData(product);
+	}
 	
 	String isrecurring = product.getValue("recurring");
 	String nextbillon = product.getValue("nextbillon.value");
