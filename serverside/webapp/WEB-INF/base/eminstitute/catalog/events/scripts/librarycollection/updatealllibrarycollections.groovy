84fda770dedc48385fc03b69d1ff5a88c6dce882
Cristobal <cristobal.mejia@gmail.com>
2024-08-20 16:30:13 -0600

    more OI

---
+++ serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/librarycollection/updatealllibrarycollections.groovy
@@ -0,0 +1,28 @@
+package librarycollection
+
+import org.entermediadb.asset.Category
+import org.entermediadb.asset.MediaArchive
+import org.openedit.Data
+import org.openedit.data.Searcher
+import org.openedit.hittracker.HitTracker
+
+
+public void init() {
+	MediaArchive mediaArchive = (MediaArchive)context.getPageValue("mediaarchive");
+	
+	HitTracker all = mediaArchive.query("librarycollection").search();
+	all.each {
+		Data librarycol = it;
+		if(librarycol.get("communitytagcategory") == null) {
+		
+		librarycol.setValue("communitytagcategory", "1" );
+		librarycol.setValue("communitytag", "oicommunity" );
+		mediaArchive.getSearcher("librarycollection").saveData(librarycol, null);
+	    log.info("saving library $librarycol");
+			}
+		
+    }
+}
+
+init();
+
