b017b22eb5af1dec8935614a72fb7be50ddc6696
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-01-13 01:40:44 +0600

    Enhance documentation layout and styling with responsive sidebar and improved content organization

--- serverside/webapp/WEB-INF/base/eminstitute/community/default/theme/js/docs.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/default/theme/js/docs.js
@@ -1,60 +1,91 @@
-"use strict";
-var sidebarToggler = document.getElementById("docs-sidebar-toggler");
-var sidebar = document.getElementById("docs-sidebar");
-var sidebarLinks = document.querySelectorAll("#docs-sidebar .scrollto");
-
-window.onload = function () {
-	responsiveSidebar();
-};
-
-window.onresize = function () {
-	responsiveSidebar();
-};
-
-function responsiveSidebar() {
-	var w = window.innerWidth;
-	if (w >= 1200) {
-		sidebar.classList.remove("sidebar-hidden");
-		sidebar.classList.add("sidebar-visible");
-	} else {
-		console.log("smaller");
-		sidebar.classList.remove("sidebar-visible");
-		sidebar.classList.add("sidebar-hidden");
+$(document).ready(function () {
+	function addSections() {
+		var section = 1;
+		var chapter = 1;
+		$("#docs-wrapper h1, #docs-wrapper h2").each(function () {
+			var text = $(this).text().replace(/\s/g, "");
+			if (!text.length) {
+				return;
+			}
+			if ($(this).is("h1")) {
+				if (!text.startsWith(section + ".")) {
+					$(this).prepend("<span>" + section + ". </span>");
+				}
+				$(this).attr("id", "section-" + section);
+				section++;
+				chapter = 1;
+			} else {
+				if (!text.startsWith(section - 1 + "." + chapter + ".")) {
+					$(this).prepend(
+						"<span>" + (section - 1) + "." + chapter + ". </span>"
+					);
+				}
+				$(this).attr("id", "section-" + section + "-" + chapter);
+				chapter++;
+			}
+		});
+		section = 1;
+		chapter = 1;
+		$("#docs-nav > ul > li.nav-item").each(function () {
+			if ($(this).hasClass("section-title")) {
+				$(this)
+					.find("a")
+					.attr("href", "#section-" + section);
+				section++;
+				chapter = 1;
+			} else {
+				$(this)
+					.find("a")
+					.attr("href", "#section-" + section + "-" + chapter);
+				chapter++;
+			}
+		});
 	}
-}
 
-sidebarToggler.addEventListener("click", () => {
-	if (sidebar.classList.contains("sidebar-visible")) {
-		console.log("visible");
-		sidebar.classList.remove("sidebar-visible");
-		sidebar.classList.add("sidebar-hidden");
-	} else {
-		console.log("hidden");
-		sidebar.classList.remove("sidebar-hidden");
-		sidebar.classList.add("sidebar-visible");
+	addSections();
+
+	var sidebar = $("#docs-sidebar");
+	function responsiveSidebar() {
+		if (window.innerWidth >= 1200) {
+			sidebar.removeClass("sidebar-hidden");
+			sidebar.addClass("sidebar-visible");
+		} else {
+			sidebar.removeClass("sidebar-visible");
+			sidebar.addClass("sidebar-hidden");
+		}
 	}
-});
+	responsiveSidebar();
 
-sidebarLinks.forEach((sidebarLink) => {
-	sidebarLink.addEventListener("click", (e) => {
-		e.preventDefault();
+	$(window).on("resize", function () {
+		responsiveSidebar();
+	});
 
-		var target = sidebarLink.getAttribute("href").replace("#", "");
-
-		document.getElementById(target).scrollIntoView({ behavior: "smooth" });
-
-		if (
-			sidebar.classList.contains("sidebar-visible") &&
-			window.innerWidth < 1200
-		) {
-			sidebar.classList.remove("sidebar-visible");
-			sidebar.classList.add("sidebar-hidden");
+	$("#docs-sidebar-toggler").on("click", () => {
+		if (sidebar.hasClass("sidebar-visible")) {
+			sidebar.removeClass("sidebar-visible");
+			sidebar.addClass("sidebar-hidden");
+		} else {
+			sidebar.removeClass("sidebar-hidden");
+			sidebar.addClass("sidebar-visible");
 		}
 	});
-});
 
-var spy = new Gumshoe("#docs-nav a", {
-	offset: 69,
-});
+	$("#docs-nav a.nav-link.scrollto").on("click", (e) => {
+		e.preventDefault();
+		var target = $(e.target).attr("href");
+		if (!target) return;
+		var scrollTop = $(target).offset().top - 72;
+		window.scrollTo({ behavior: "smooth", top: scrollTop });
 
-var lightbox = new SimpleLightbox(".simplelightbox-gallery a", {});
+		if (sidebar.hasClass("sidebar-visible") && window.innerWidth < 1200) {
+			sidebar.removeClass("sidebar-visible");
+			sidebar.addClass("sidebar-hidden");
+		}
+	});
+
+	new Gumshoe("#docs-nav a", {
+		offset: 150,
+	});
+
+	new SimpleLightbox("#docs-wrapper img", { sourceAttr: "src" });
+});
