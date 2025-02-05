608c92af70d1027ee19c4e3713ad0833aef3079f
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-02-06 01:03:29 +0600

    Refactor layout and navigation: update footer links, remove masonry scripts, and enhance portfolio grid with carousel functionality

--- serverside/webapp/WEB-INF/base/eminstitute/community/luma-adspace/theme/js/script.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/luma-adspace/theme/js/script.js
@@ -13,11 +13,18 @@ $(".faq-item h3, .faq-item .faq-toggle").click(function () {
 	}
 });
 
-var $grid = $(".masonry-grid").masonry({
-	itemSelector: ".grid-item",
+// var $grid = $(".masonry-grid").masonry({
+// 	itemSelector: ".grid-item",
+// });
+
+// $grid.imagesLoaded().progress(function (_, { img }) {
+// 	$grid.masonry("layout");
+// 	$(img).siblings(".img-loader").remove();
+// });
+$(".portfolio-grid a").on("mouseover", function () {
+	$(".portfolio-grid").addClass("pause");
 });
 
-$grid.imagesLoaded().progress(function (_, { img }) {
-	$grid.masonry("layout");
-	$(img).siblings(".img-loader").remove();
+$(".portfolio-grid a").on("mouseout", function () {
+	$(".portfolio-grid").removeClass("pause");
 });
