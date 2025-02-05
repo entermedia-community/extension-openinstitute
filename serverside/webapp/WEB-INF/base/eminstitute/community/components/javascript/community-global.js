cea9eb23d79979180c5a6ad5b2381ec08b58360f
Cristobal <cristobal.mejia@gmail.com>
2025-02-05 17:04:26 -0600

    select2 tags component JS missing

--- serverside/webapp/WEB-INF/base/eminstitute/community/components/javascript/community-global.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/components/javascript/community-global.js
@@ -84,4 +84,141 @@ $(document).ready(function () {
 			);
 		});
 	});
+	
+	
+	
+	//Select2 components
+	
+	function select2formatResult(emdata) {
+			/*
+			 * var element = $(this.element); var showicon =
+			 * element.data("showicon"); if( showicon ) { var type =
+			 * element.data("searchtype"); var html = "<img
+			 * class='autocompleteicon' src='" + themeprefix + "/images/icons/" +
+			 * type + ".png'/>" + emdata.name; return html; } else { return
+			 * emdata.name; }
+			 */
+			return emdata.name;
+		}
+		function select2Selected(selectedoption) {
+			// "#list-" + foreignkeyid
+			// var id = container.closest(".select2-container").attr("id");
+			// id = "list-" + id.substring(5); //remove sid2_
+			// container.closest("form").find("#" + id ).val(emdata.id);
+			return selectedoption.name || selectedoption.text;
+		}
+		
+		
+	lQuery("select.listtags").livequery(function () {
+			var theinput = $(this);
+			var dropdownParent = theinput.data("dropdownparent");
+			if (dropdownParent && $("#" + dropdownParent).length) {
+				dropdownParent = $("#" + dropdownParent);
+			} else {
+				dropdownParent = $(this).parent();
+			}
+			var parent = theinput.closest("#main-media-container");
+			if (parent.length) {
+				dropdownParent = parent;
+			}
+			var parent = theinput.parents(".modal-content");
+			if (parent.length) {
+				dropdownParent = parent;
+			}
+			var searchtype = theinput.data("searchtype");
+			var searchfield = theinput.data("searchfield");
+			var catalogid = theinput.data("listcatalogid");
+			var sortby = theinput.data("sortby");
+			var defaulttext = theinput.data("showdefault");
+			if (!defaulttext) {
+				defaulttext = "Search";
+			}
+			var allowClear = $(this).data("allowclear");
+			if (allowClear == undefined) {
+				allowClear = true;
+			}
+
+			var url = theinput.data("url");
+			if (!url) {
+				url = apphome + "/components/xml/types/autocomplete/tagsearch.txt";
+			}
+
+			if ($.fn.select2) {
+				theinput.select2({
+					tags: true,
+					placeholder: defaulttext,
+					allowClear: allowClear,
+					dropdownParent: dropdownParent,
+					selectOnBlur: true,
+					delay: 150,
+					minimumInputLength: 1,
+					ajax: {
+						// instead of writing
+						// the function to
+						// execute the request
+						// we use Select2's
+						// convenient helper
+						url: url,
+						xhrFields: {
+							withCredentials: true,
+						},
+						crossDomain: true,
+						dataType: "json",
+						data: function (params) {
+							var search = {
+								page_limit: 15,
+								page: params.page,
+							};
+							search["field"] = searchfield;
+							search["operation"] = "contains";
+							search["searchtype"] = searchtype;
+							search[searchfield + ".value"] = params.term; // search
+							// term
+							search["sortby"] = sortby; // search
+							// term
+							return search;
+						},
+						processResults: function (data, params) {
+							// parse the
+							// results
+							// into the
+							// format
+							// expected
+							// by
+							// Select2.
+							params.page = params.page || 1;
+							return {
+								results: data.rows,
+								pagination: {
+									more: false,
+									// (params.page * 30) <
+									// data.total_count
+								},
+							};
+						},
+					},
+					escapeMarkup: function (m) {
+						return m;
+					},
+					templateResult: select2formatResult,
+					templateSelection: select2Selected,
+					tokenSeparators: ["|"],
+					separator: "|",
+				});
+			}
+			theinput.on("select2:select", function () {
+				if ($(this).parents(".ignore").length == 0) {
+					$(this).valid();
+				}
+			});
+			theinput.on("select2:unselect", function () {
+				if ($(this).parents(".ignore").length == 0) {
+					$(this).valid();
+				}
+			});
+		});
+	
+	
+	
+	
 });
