bc978afc167c20f26ce9f1ffc67fc148c08b4ab2
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-02-09 09:28:51 +0600

    Add new images, HTML templates, and JavaScript for MetaVault project

---
+++ serverside/webapp/WEB-INF/base/eminstitute/community/metavault/theme/js/script.js
@@ -0,0 +1,14 @@
+AOS.init({
+	duration: 600,
+	easing: "ease-in-out",
+	once: true,
+	mirror: false,
+});
+
+$(".faq-item h3, .faq-item .faq-toggle").click(function () {
+	var isActive = $(this).parent().hasClass("faq-active");
+	$(".faq-item").removeClass("faq-active");
+	if (!isActive) {
+		$(this).parent().toggleClass("faq-active");
+	}
+});
