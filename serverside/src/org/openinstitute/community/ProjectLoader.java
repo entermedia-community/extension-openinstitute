5ecc461cd92a6be2ae1e471c8e0dca6f911ebfe9
Christopher Burkey <cburkey@entermediadb.org>
2025-02-17 12:30:47 -0600

    Added log

--- serverside/src/org/openinstitute/community/ProjectLoader.java
+++ serverside/src/org/openinstitute/community/ProjectLoader.java
@@ -214,9 +214,9 @@ public class ProjectLoader implements PageLoader, CatalogEnabled
 		}
 		else {
 			//log.info("Couldn't find Collection: " + secondpart);
-
-			
 		}
+		
+		log.info("Couldn't find any content: Orinal path: " + requestedPath + " Community Home:" + communityhome );
 		return null;
 	}
 
