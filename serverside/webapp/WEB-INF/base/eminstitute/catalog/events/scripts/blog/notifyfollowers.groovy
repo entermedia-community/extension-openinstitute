ad6a576a1811f8cd8f962363bf42529139f9009d
Cristobal <cristobal.mejia@gmail.com>
2025-03-12 13:07:10 -0600

    Send Notification

---
+++ serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/blog/notifyfollowers.groovy
@@ -0,0 +1,128 @@
+package blog;
+
+import org.apache.commons.collections.map.HashedMap
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.email.WebEmail
+import org.openedit.*
+import org.openedit.hittracker.HitTracker
+import org.openedit.users.User
+
+//Chat Notifications
+
+public void init()
+{
+	MediaArchive archive = (MediaArchive)context.getPageValue("mediaarchive");
+	
+	//Search Post to publish
+	
+	HitTracker posttopublish = archive.query("userpost").exact("poststatus", "readytopublish").search();
+	
+	for (Iterator iterator = posttopublish.iterator(); iterator.hasNext();) {
+		MultiValued userpost =  (MultiValued) iterator.next();
+		try 
+		{
+			log.info("Sending Notifications for: " +userpost);
+			notifyfollowers(userpost.getId(), userpost.get("librarycollection"));
+			userpost.setValue("poststatus", "published");
+			
+		}
+		catch (Throwable e)
+		{
+			log.error("Error sending notifications: " + userpost.get("name"), e);
+			userpost.setValue("poststatus", "error");
+		}
+		archive.getSearcher("userposts").saveData(userpost);
+	}
+	
+}
+
+
+
+
+public void notifyfollowers(String userpostid, String collectionid)
+{
+	MediaArchive archive = context.getPageValue("mediaarchive");
+	
+	HitTracker teammembers = archive.query("librarycollectionusers").exact("ontheteam", "true").exact("collectionid", collectionid).search();
+	HitTracker followers = archive.query("librarycollectionlikes").exact("collectionid", collectionid).search();
+	if (followers == null)
+	{
+		log.info("No Followers in this Project. " + collectionid)
+		return;  //No followers
+	}
+	
+	HashMap<String, User> notifyusers = new HashedMap();
+	
+	//loop Team Members
+	for (Iterator followerIterator = teammembers.iterator(); followerIterator.hasNext();) {
+		MultiValued follower = (MultiValued)followerIterator.next();
+		if (notifyusers.get(follower.get("followeruser")) != null)
+		{
+			continue;
+		}
+		User user = archive.getUser(follower.get("followeruser"));
+		notifyusers.put(follower.get("followeruser"), user);
+	}
+
+	
+	//loop likes
+	for (Iterator followerIterator2 = followers.iterator(); followerIterator2.hasNext();) {
+		MultiValued follower = (MultiValued)followerIterator2.next();
+		if (notifyusers.get(follower.get("followeruser")) != null)
+		{
+			continue;
+		}
+		User user = archive.getUser(follower.get("followeruser"));
+		notifyusers.put(follower.get("followeruser"), user);
+	} 
+	
+	log.info("Notify Users: " + notifyusers);
+	
+	Data collection = archive.getCachedData("librarycollection", collectionid);
+	Data community = archive.getCachedData("communitytagcategory", collection.get("communitytagcategory"));
+	
+	
+	if (collection == null || community == null)
+	{
+		log.error("Skiping post: " + userpostid + " No Collection or Community " + collection + " " + community);
+	}
+	
+	String emailSubject = "[OI]" + community.getName() + ": Added to Team";
+	String siteid = context.findValue("siteid");
+	String template = siteid + community.get("templatepath") + "/theme/emails/newpostnotifyfollowers.html";
+	
+	Iterator<String> keys = notifyusers.keySet().iterator();
+	while(keys.hasNext())
+	{
+		String key = keys.next();
+		User user = notifyusers.get(key);
+			
+		WebEmail templatemail = archive.createSystemEmail(user, template);
+		templatemail.setSubject(emailSubject); //TODO: Translate
+		Map objects = new HashMap();
+		String entermediakey = archive.userManager.getEnterMediaKey(user);
+		objects.put("entermediakey",entermediakey);
+		objects.put("teamuser",user);
+		objects.put("librarycol", collection);
+		objects.put("apphome", context.getPageValue("apphome"));
+		objects.put("applink", context.getPageValue("applink"));
+		objects.put("siteroot", getSiteRoot());
+		objects.put("communityhome", community.get("templatepath"));
+		objects.put("community",community);
+			
+		//	templatemail.send(objects);
+	}
+}
+
+private String getSiteRoot() {
+	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
+	String site = mediaArchive.getCatalogSettingValue("siteroot");
+	if (!site) {
+		site = mediaArchive.getCatalogSettingValue("cdn_prefix");
+	}
+	return site;
+}
+
+
+
+init();
--- serverside/webapp/WEB-INF/base/eminstitute/community/default/project/channel/uploadsave.groovy
+++ serverside/webapp/WEB-INF/base/eminstitute/community/default/project/channel/uploadsave.groovy
@@ -22,6 +22,7 @@ public void init()
 		
 }
 
+
 init();
 
 
