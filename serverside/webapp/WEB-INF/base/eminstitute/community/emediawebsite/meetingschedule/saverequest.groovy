0dbd119867e229fe71b2cdd15f3c35f685b14675
Cristobal <cristobal.mejia@gmail.com>
2025-04-21 18:29:35 -0600

    website changes

--- serverside/webapp/WEB-INF/base/eminstitute/community/emediawebsite/meetingschedule/saverequest.groovy
+++ serverside/webapp/WEB-INF/base/eminstitute/community/emediawebsite/meetingschedule/saverequest.groovy
@@ -11,7 +11,9 @@ import org.entermediadb.email.TemplateWebEmail
 public void init() {
 
 	//Notify to Email:
-	String notifyemail = "sales@entermediadb.org";  //get it from catalog settings?
+	//String notifyemail = "sales@entermediadb.org";  //get it from catalog settings?
+	
+	String notifyemail = "cristobal@entermediadb.org";  //get it from catalog settings?
 
 	//prevent re-submition
 	 String clientform = context.getSessionValue("clientform");
