e7a67980392fb611426955b66a0507a1359d8524
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-07-26 07:44:33 +0600

    impact bank added

--- serverside/webapp/WEB-INF/base/eminstitute/community/impact-bank/theme/js/impact-bank.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/impact-bank/theme/js/impact-bank.js
@@ -1,51 +1,39 @@
-// initialization of header
-var header = new HSHeader($("#header")).init();
+$(document).ready(function () {
+  // initialization of header
+  var header = new HSHeader($("#header")).init();
 
-// initialization of mega menu
-var megaMenu = new HSMegaMenu($(".js-mega-menu"), {
-  desktop: {
-    position: "left",
-  },
-}).init();
+  // initialization of slick carousel
+  $(".js-slick-carousel").each(function () {
+    var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+  });
 
-var unfold = new HSUnfold(".js-hs-unfold-invoker").init();
+  // initialization of video on background
+  $(".js-video-bg").each(function () {
+    var videoBg = new HSVideoBg($(this)).init();
+  });
 
-// initialization of slick carousel
-$(".js-slick-carousel").each(function () {
-  var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+  $(".js-text-animation").text("");
+  var typed = new Typed(".js-text-animation", {
+    strings: ["Innovation", "Ideas", "Solutions", "Success"],
+    typeSpeed: 70,
+    loop: true,
+    backSpeed: 40,
+    backDelay: 2000,
+  });
+
+  // initialization of text animation (typing)
+
+  // initialization of form validation
+  $(".js-validate").each(function () {
+    var validation = $.HSCore.components.HSValidation.init($(this));
+  });
+
+  // initialization of aos
+  AOS.init({
+    duration: 650,
+    once: true,
+  });
 });
-
-// initialization of video on background
-$(".js-video-bg").each(function () {
-  var videoBg = new HSVideoBg($(this)).init();
-});
-
-// initialization of text animation (typing)
-var typed = new Typed(".js-text-animation", {
-  strings: ["Innovation", "Ideas", "Solutions", "Success"],
-  typeSpeed: 70,
-  loop: true,
-  backSpeed: 40,
-  backDelay: 2000,
-});
-console.log(typed);
-
-// initialization of form validation
-$(".js-validate").each(function () {
-  var validation = $.HSCore.components.HSValidation.init($(this));
-});
-
-// initialization of aos
-AOS.init({
-  duration: 650,
-  once: true,
-});
-
-// initialization of go to
-$(".js-go-to").each(function () {
-  var goTo = new HSGoTo($(this)).init();
-});
-
 if (/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
   document.write(
     '<script src="$communityhome/assets/vendor/polifills.js"></script>'
