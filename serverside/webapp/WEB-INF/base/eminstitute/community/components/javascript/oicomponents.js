038e933995f65d4b2bd1b803b1f849eb6fae9ac7
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-07-29 00:05:32 +0600

    projects fixed

--- serverside/webapp/WEB-INF/base/eminstitute/community/components/javascript/oicomponents.js
+++ serverside/webapp/WEB-INF/base/eminstitute/community/components/javascript/oicomponents.js
@@ -1,37 +1,38 @@
-jQuery(function () {
-	var lastTypeahead;
-	var searchPopupOpened = false;
-	var searchInputEmpty = $('#searchInputEmpty');
-	var searchLoading = $('#searchLoading');
-	var searchNoResult = $('#searchNoResult');
-	var searchResult = $('#searchResult');
-	var searchResultPlaceholder = $('#searchResultPlaceholder');
+$(document).ready(function () {
+  var lastTypeahead;
+  var searchPopupOpened = false;
+  var searchInputEmpty = $("#searchInputEmpty");
+  var searchLoading = $("#searchLoading");
+  var searchNoResult = $("#searchNoResult");
+  var searchResult = $("#searchResult");
+  var searchResultPlaceholder = $("#searchResultPlaceholder");
 
-	function openSearchPopup() {
-		$(".search-popup").css("display", "flex")
-		$("#searchInput").focus();
-		searchPopupOpened = true;
-	}
+  function openSearchPopup() {
+    $(".search-popup").css("display", "flex");
+    $("#searchInput").focus();
+    searchPopupOpened = true;
+  }
 
-	$("#searchBtn").on("click", openSearchPopup);
+  $("#searchBtn").on("click", openSearchPopup);
+  $(".searchBtn").on("click", openSearchPopup);
 
-	function closeSearchPopup() {
-		$(".search-popup").css("display", "none");
-		searchPopupOpened = false;
-	}
+  function closeSearchPopup() {
+    $(".search-popup").css("display", "none");
+    searchPopupOpened = false;
+  }
 
-	$("#closeSearch").on("click", closeSearchPopup);
+  $("#closeSearch").on("click", closeSearchPopup);
 
-	$(".search-popup").on("click", function (e) {
-		if (e.target !== this) return;
-		closeSearchPopup();
-	});
+  $(".search-popup").on("click", function (e) {
+    if (e.target !== this) return;
+    closeSearchPopup();
+  });
 
-	$(document).on("keyup", function (e) {
-		if (e.key === 'Escape' && searchPopupOpened) {
-			closeSearchPopup();
-		}
-	});
+  $(document).on("keyup", function (e) {
+    if (e.key === "Escape" && searchPopupOpened) {
+      closeSearchPopup();
+    }
+  });
 
   lQuery(".trim-text").livequery(function () {
     var maxLength = $(this).data("max");
@@ -43,58 +44,58 @@ jQuery(function () {
     $(this).append('<button class="see-more">(...see more)</button>');
   });
 
-	lQuery("#searchInput").livequery(function () {
-		var input = $(this);
-		var options = input.data();
+  lQuery("#searchInput").livequery(function () {
+    var input = $(this);
+    var options = input.data();
 
-		input.on("keyup", function (e) {
-			if(e.key.length > 1 || e.key === ' ') {
-				return;
-			}
-			var query = input.val().trim();
-			if (!query || query.length < 2) {
-				searchInputEmpty.css("display", "block");
-				searchLoading.css("display", "none");
-				searchNoResult.css("display", "none");
-				searchResult.html('');
-				searchResultPlaceholder.css("display", "block");
-				return;
-			} else {
-				searchInputEmpty.css("display", "none");
-			}
+    input.on("keyup", function (e) {
+      if (e.key.length > 1 || e.key === " ") {
+        return;
+      }
+      var query = input.val().trim();
+      if (!query || query.length < 2) {
+        searchInputEmpty.css("display", "block");
+        searchLoading.css("display", "none");
+        searchNoResult.css("display", "none");
+        searchResult.html("");
+        searchResultPlaceholder.css("display", "block");
+        return;
+      } else {
+        searchInputEmpty.css("display", "none");
+      }
 
-			var loaderTimeout = setTimeout(function () {
-				searchLoading.css("display", "flex");
-			}, 250);
+      var loaderTimeout = setTimeout(function () {
+        searchLoading.css("display", "flex");
+      }, 250);
 
-			options["description.value"] = query;
+      options["description.value"] = query;
 
-			if (lastTypeahead) {
-				lastTypeahead.abort();
-			}
+      if (lastTypeahead) {
+        lastTypeahead.abort();
+      }
 
-			lastTypeahead = $.ajax({
-				url: options.typeaheadurl,
-				async: true,
-				data: options,
-				timeout: 5000,
-				success: function (data) {
-					data = data.trim();
-					if (data) {
-						searchResultPlaceholder.css("display", "none");
-						searchResult.html(data);
-						searchNoResult.css("display", "none");
-					} else {
-						searchResult.html('');
-						searchNoResult.css("display", "block");
-						searchResultPlaceholder.css("display", "block");
-					}
-				},
-				complete: function () {
-					clearTimeout(loaderTimeout);
-					searchLoading.css("display", "none");
-				}
-			});
-		});
-	});
+      lastTypeahead = $.ajax({
+        url: options.typeaheadurl,
+        async: true,
+        data: options,
+        timeout: 5000,
+        success: function (data) {
+          data = data.trim();
+          if (data) {
+            searchResultPlaceholder.css("display", "none");
+            searchResult.html(data);
+            searchNoResult.css("display", "none");
+          } else {
+            searchResult.html("");
+            searchNoResult.css("display", "block");
+            searchResultPlaceholder.css("display", "block");
+          }
+        },
+        complete: function () {
+          clearTimeout(loaderTimeout);
+          searchLoading.css("display", "none");
+        },
+      });
+    });
+  });
 });
