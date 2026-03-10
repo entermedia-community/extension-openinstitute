d869b18742fe55a4cc2429cf9243d62d01c9fb04
Christopher Burkey <cburkey@entermediadb.org>
2026-03-10 13:24:06 -0600

    Update

--- serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/librarycollection/importcollections.groovy
+++ serverside/webapp/WEB-INF/base/eminstitute/catalog/events/scripts/librarycollection/importcollections.groovy
@@ -2,20 +2,19 @@ import org.entermediadb.asset.MediaArchive
 import org.entermediadb.projects.*
 import org.openedit.*
 import org.openedit.hittracker.*
+import org.openedit.util.HttpSharedConnection
 import org.openedit.data.*
 
 public void init()
 {
 	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
+
+	HttpSharedConnection connection = new HttpSharedConnection();
+	String baseurl = "https://openinstitute.org/site/mediadb";
+	connection.getJson(baseurl + "/")
+	
 	Searcher searcher = mediaArchive.getSearcher("librarycollection");
-	HitTracker all = mediaArchive.query("librarycollection").all().search();
-	List tosave = new ArrayList();
-	for(Data data in all)
-	{
-		LibraryCollection collection = searcher.loadData(data);
-		collection.setValue("name", collection.getName());
-		tosave.add(collection);
-	}	
+
 	searcher.saveAllData(tosave,null);
 	log.info("saved ${tosave.size()}");
 }
